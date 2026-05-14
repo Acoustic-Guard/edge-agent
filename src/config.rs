17930519fafc9158822bcef033c68720930602c6
handziurdmytro <handziurdmytro@gmail.com>
2026-05-14 23:39:22 +0300

    feat(config): add app config with hub_url, device_ad and interval_secs

--- src/config.rs
+++ src/config.rs
@@ -1,1 +1,33 @@
+use anyhow::{Context, Result};
+use dotenvy::dotenv;
+use std::env;
 
+#[derive(Debug, Clone)]
+pub struct AppConfig {
+    pub hub_url: String,
+    pub device_id: String,
+    pub loop_interval_secs: u64,
+}
+
+impl AppConfig {
+    pub fn load() -> Result<Self> {
+        dotenv().ok();
+
+        let hub_url =
+            env::var("HUB_URL").context("variable HUB_URL is not set in .env or environment")?;
+
+        let device_id = env::var("DEVICE_ID")
+            .context("variable DEVICE_ID is not set in .env or environment")?;
+
+        let loop_interval_secs = env::var("LOOP_INTERVAL_SECS")
+            .context("variable LOOP_INTERVAL_SECS is not set in .env or environment")?
+            .parse::<u64>()
+            .context("LOOP_INTERVAL_SECS must be a valid u64 number")?;
+
+        Ok(Self {
+            hub_url,
+            device_id,
+            loop_interval_secs,
+        })
+    }
+}
