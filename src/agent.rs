062787f621c3f129fd2c55c20eae9699aa94cf03
handziurdmytro <handziurdmytro@gmail.com>
2026-05-15 00:14:15 +0300

    feat: implement agent loop and start it from main.rs

--- src/agent.rs
+++ src/agent.rs
@@ -1,1 +1,59 @@
+use crate::config::AppConfig;
+use crate::transport::dto::SpectrumPayloadDto;
+use crate::transport::http::HubClient;
+use crate::{audio, dsp};
+use anyhow::Result;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use tracing::{error, info, warn};
 
+pub struct AcousticAgent {
+    pub config: AppConfig,
+    pub client: HubClient,
+}
+
+impl AcousticAgent {
+    pub fn new(config: AppConfig) -> Self {
+        let client = HubClient::new(&config.hub_url);
+        Self { config, client }
+    }
+
+    pub async fn run(&self) {
+        let secs = self.config.loop_interval_secs;
+        info!("Staring the main agent loop. Interval: {secs} seconds");
+
+        loop {
+            if let Err(e) = self.process_and_send().await {
+                error!("Error in current iteration: {e}");
+                warn!("Waiting for {secs} seconds before the next attempt");
+            }
+
+            tokio::time::sleep(Duration::from_secs(secs)).await;
+        }
+    }
+
+    async fn process_and_send(&self) -> Result<()> {
+        info!("reading audio file...");
+        let frame = audio::file::read_wav_file("assets/satie.wav")?;
+
+        info!("FFT processing...");
+        let spectrum = dsp::fft::compute_fft(&frame.samples);
+
+        let compressed_spectrum: Vec<f32> = spectrum.into_iter().take(100).collect();
+        let timestamp = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs();
+        let payload = SpectrumPayloadDto {
+            device_id: self.config.device_id.clone(),
+            timestamp,
+            sample_rate: frame.sample_rate,
+            frequency_spectrum: compressed_spectrum,
+        };
+
+        info!("sending to a hub...");
+
+        match self.client.send_spectrum(&payload).await {
+            Ok(_) => info!("data was successfully delivered"),
+            Err(e) => error!("error: {e}"),
+        }
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,29 @@ mod dsp;
 mod error;
 mod transport;
 
+use crate::agent::AcousticAgent;
+use crate::config::AppConfig;
+use anyhow::Result;
+use tracing::info;
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .json()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .with_current_span(false)
+        .with_span_list(false)
+        .with_target(true)
+        .with_thread_ids(true)
+        .with_file(true)
+        .with_line_number(true)
+        .init();
+
+    info!("edge agent initialization");
+
+    let config = AppConfig::load()?;
+    let agent = AcousticAgent::new(config);
+    agent.run().await;
+
     Ok(())
 }
