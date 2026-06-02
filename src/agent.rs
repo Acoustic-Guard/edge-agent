eff798a096d297a890757c7a419756eb24c2b44d
handziurdmytro <handziurdmytro@gmail.com>
2026-06-02 15:11:27 +0300

    feat: add batching logic for telemetry

--- src/agent.rs
+++ src/agent.rs
@@ -1,14 +1,52 @@
 use anyhow::Result;
-use std::collections::VecDeque;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
-use tokio::sync::RwLock;
+use tokio::time::Instant;
 use tracing::{error, info, warn};
 
 use crate::config::AppConfig;
 use crate::transport::dto::SpectrumPayloadDto;
 use crate::transport::rmq::RmqClient;
 use crate::{audio, dsp};
+
+#[derive(Debug)]
+struct TelemetryStats {
+    sum_db: f32,
+    max_db: f32,
+    count: u32,
+}
+
+impl TelemetryStats {
+    fn new() -> Self {
+        Self {
+            sum_db: 0.0,
+            max_db: f32::MIN,
+            count: 0,
+        }
+    }
+
+    fn add(&mut self, db: f32) {
+        self.sum_db += db;
+        self.count += 1;
+        if db > self.max_db {
+            self.max_db = db;
+        }
+    }
+
+    fn take_and_reset(&mut self) -> Option<(f32, f32)> {
+        if self.count == 0 {
+            return None;
+        }
+        let avg = self.sum_db / self.count as f32;
+        let max = self.max_db;
+
+        self.sum_db = 0.0;
+        self.count = 0;
+        self.max_db = f32::MIN;
+
+        Some((avg, max))
+    }
+}
 
 pub struct AcousticAgent {
     pub config: AppConfig,
@@ -42,13 +80,11 @@ impl AcousticAgent {
     }
 
     pub async fn run(&self) {
-        info!("Starting the multi-threaded agent loop...");
-
-        let latest_bg_noise = Arc::new(RwLock::new(-100.0f32));
+        let telemetry_stats = Arc::new(Mutex::new(TelemetryStats::new()));
 
         let anomaly_config = self.config.clone();
         let anomaly_client = self.client.clone();
-        let noise_for_anomaly = Arc::clone(&latest_bg_noise);
+        let stats_for_anomaly = Arc::clone(&telemetry_stats);
 
         let anomaly_task = tokio::spawn(async move {
             let frame = match audio::file::read_wav_file(&anomaly_config.audio_file_path) {
@@ -60,13 +96,12 @@ impl AcousticAgent {
             };
 
             let sample_rate = frame.sample_rate;
-            let chunk_size = (sample_rate as f32 * 0.1) as usize;
-            let window_size = sample_rate as usize;
-
+            let chunk_size = sample_rate as usize;
             let mut offset = 0;
-            let mut rolling_window: VecDeque<i16> = VecDeque::with_capacity(window_size);
 
-            let mut interval = tokio::time::interval(Duration::from_millis(1000));
+            let mut interval = tokio::time::interval(Duration::from_millis(3000));
+            let mut last_anomaly_time: Option<Instant> = None;
+            let cooldown_duration = Duration::from_millis(3000);
 
             loop {
                 interval.tick().await;
@@ -77,13 +112,6 @@ impl AcousticAgent {
                 } else {
                     &frame.samples[offset..]
                 };
-
-                for &sample in chunk {
-                    if rolling_window.len() >= window_size {
-                        rolling_window.pop_front();
-                    }
-                    rolling_window.push_back(sample);
-                }
 
                 offset += chunk_size;
                 if offset >= frame.samples.len() {
@@ -96,35 +124,46 @@ impl AcousticAgent {
 
                 let peak_db = dsp::features::compute_dbfs(chunk);
 
-                *noise_for_anomaly.write().await = peak_db;
+                {
+                    let mut stats = stats_for_anomaly.lock().unwrap();
+                    stats.add(peak_db);
+                }
 
                 if peak_db > -30.0 {
-                    info!(
-                        "Anomaly detected! Peak DB: {:.2}. Calculating FFT...",
-                        peak_db
-                    );
-
-                    let window_vec: Vec<i16> = rolling_window.iter().copied().collect();
-                    let spectrum = dsp::fft::compute_fft(&window_vec);
-
-                    let timestamp_ms = SystemTime::now()
-                        .duration_since(UNIX_EPOCH)
-                        .unwrap()
-                        .as_millis() as u64;
-
-                    let payload = SpectrumPayloadDto {
-                        sensor_id: anomaly_config.device_id.clone(),
-                        captured_at_ms: timestamp_ms,
-                        latitude: anomaly_config.latitude,
-                        longitude: anomaly_config.longitude,
-                        fft_bins: spectrum.into_iter().take(100).collect(),
-                        sample_rate_hz: sample_rate,
-                        peak_db,
+                    let now = Instant::now();
+                    let should_send = match last_anomaly_time {
+                        Some(time) => now.duration_since(time) >= cooldown_duration,
+                        None => true,
                     };
 
-                    let routing_key = format!("sensor.anomaly.{}", anomaly_config.device_id);
-                    if let Err(e) = anomaly_client.send_message(&routing_key, &payload).await {
-                        error!("Failed to send anomaly: {}", e);
+                    if should_send {
+                        info!(
+                            "Anomaly detected! Peak DB: {:.2}. Calculating FFT...",
+                            peak_db
+                        );
+
+                        let spectrum = dsp::fft::compute_fft(chunk);
+                        let timestamp_ms = SystemTime::now()
+                            .duration_since(UNIX_EPOCH)
+                            .unwrap()
+                            .as_millis() as u64;
+
+                        let payload = SpectrumPayloadDto {
+                            sensor_id: anomaly_config.device_id.clone(),
+                            captured_at_ms: timestamp_ms,
+                            latitude: anomaly_config.latitude,
+                            longitude: anomaly_config.longitude,
+                            fft_bins: spectrum.into_iter().take(100).collect(),
+                            sample_rate_hz: sample_rate,
+                            peak_db,
+                        };
+
+                        let routing_key = format!("sensor.anomaly.{}", anomaly_config.device_id);
+                        if let Err(e) = anomaly_client.send_message(&routing_key, &payload).await {
+                            error!("Failed to send anomaly: {}", e);
+                        }
+
+                        last_anomaly_time = Some(now);
                     }
                 }
             }
@@ -132,8 +171,9 @@ impl AcousticAgent {
 
         let telemetry_config = self.config.clone();
         let telemetry_client = self.client.clone();
-        let noise_for_telemetry = Arc::clone(&latest_bg_noise);
-        let telemetry_interval = self.config.loop_interval_secs;
+        let stats_for_telemetry = Arc::clone(&telemetry_stats);
+
+        let telemetry_interval = /*300*/ 20;
 
         let telemetry_task = tokio::spawn(async move {
             let mut interval = tokio::time::interval(Duration::from_secs(telemetry_interval));
@@ -141,28 +181,36 @@ impl AcousticAgent {
             loop {
                 interval.tick().await;
 
-                let current_noise = *noise_for_telemetry.read().await;
-
-                let timestamp_ms = SystemTime::now()
-                    .duration_since(UNIX_EPOCH)
-                    .unwrap()
-                    .as_millis() as u64;
-
-                let payload = SpectrumPayloadDto {
-                    sensor_id: telemetry_config.device_id.clone(),
-                    captured_at_ms: timestamp_ms,
-                    latitude: telemetry_config.latitude,
-                    longitude: telemetry_config.longitude,
-                    fft_bins: vec![],
-                    sample_rate_hz: 44100,
-                    peak_db: current_noise,
+                let current_stats = {
+                    let mut stats = stats_for_telemetry.lock().unwrap();
+                    stats.take_and_reset()
                 };
 
-                info!("Sending Telemetry. Current Peak DB: {:.2}", current_noise);
+                if let Some((avg_db, max_db)) = current_stats {
+                    let timestamp_ms = SystemTime::now()
+                        .duration_since(UNIX_EPOCH)
+                        .unwrap()
+                        .as_millis() as u64;
 
-                let routing_key = format!("sensor.telemetry.{}", telemetry_config.device_id);
-                if let Err(e) = telemetry_client.send_message(&routing_key, &payload).await {
-                    error!("Failed to send telemetry: {}", e);
+                    let payload = SpectrumPayloadDto {
+                        sensor_id: telemetry_config.device_id.clone(),
+                        captured_at_ms: timestamp_ms,
+                        latitude: telemetry_config.latitude,
+                        longitude: telemetry_config.longitude,
+                        fft_bins: vec![],
+                        sample_rate_hz: 44100,
+                        peak_db: avg_db,
+                    };
+
+                    info!(
+                        "Sending 5-Min Telemetry. Avg DB: {:.2}, Max DB: {:.2}",
+                        avg_db, max_db
+                    );
+
+                    let routing_key = format!("sensor.telemetry.{}", telemetry_config.device_id);
+                    if let Err(e) = telemetry_client.send_message(&routing_key, &payload).await {
+                        error!("Failed to send telemetry: {}", e);
+                    }
                 }
             }
         });
